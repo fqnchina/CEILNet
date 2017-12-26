bca68581bc90a59c91b1d752b7baa50c0a2103ad
Fan Qingnan <fqnchina@gmail.com>
2017-12-26 11:46:49 +0800

    adam_state

---
+++ adam_state.lua
@@ -0,0 +1,60 @@
+--[[ An implementation of Adam http://arxiv.org/pdf/1412.6980.pdf
+
+ARGS:
+
+- 'opfunc' : a function that takes a single input (X), the point
+             of a evaluation, and returns f(X) and df/dX
+- 'x'      : the initial point
+- 'config` : a table with configuration parameters for the optimizer
+- 'config.learningRate'      : learning rate
+- 'config.beta1'             : first moment coefficient
+- 'config.beta2'             : second moment coefficient
+- 'config.epsilon'           : for numerical stability
+- 'state'                    : a table describing the state of the optimizer; after each
+                              call the state is modified
+
+RETURN:
+- `x`     : the new x vector
+- `f(x)`  : the function, evaluated before the update
+
+]]
+
+function optim.adam_state(opfunc, x, config, state)
+    -- (0) get/update state
+    local config = config or {}
+    local state = state or config
+    local lr = config.learningRate or 0.001
+
+    local beta1 = config.beta1 or 0.9
+    local beta2 = config.beta2 or 0.999
+    local epsilon = config.epsilon or 1e-8
+
+    -- (1) evaluate f(x) and df/dx
+    local fx, dfdx = opfunc(x)
+
+    -- Initialization
+    state.t = state.t or 0
+    -- Exponential moving average of gradient values
+    state.m = state.m or x.new(dfdx:size()):zero()
+    -- Exponential moving average of squared gradient values
+    state.v = state.v or x.new(dfdx:size()):zero()
+    -- A tmp tensor to hold the sqrt(v) + epsilon
+    state.denom = state.denom or x.new(dfdx:size()):zero()
+
+    state.t = state.t + 1
+    
+    -- Decay the first and second moment running average coefficient
+    state.m:mul(beta1):add(1-beta1, dfdx)
+    state.v:mul(beta2):addcmul(1-beta2, dfdx, dfdx)
+
+    state.denom:copy(state.v):sqrt():add(epsilon)
+
+    local biasCorrection1 = 1 - beta1^state.t
+    local biasCorrection2 = 1 - beta2^state.t
+    local stepSize = lr * math.sqrt(biasCorrection2)/biasCorrection1
+    -- (2) update x
+    x:addcdiv(-stepSize, state.m, state.denom)
+
+    -- return x*, f(x) before optimization
+    return x, {fx}, state
+end
